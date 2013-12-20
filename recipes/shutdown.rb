d865c3dbea746c15b779f8e02adab94f138697ff
Kyle Gochenour <kyle.gochenour@spinen.com>
2013-12-20 11:40:29 -0500

    moar update

--- recipes/shutdown.rb
+++ recipes/shutdown.rb
@@ -1,7 +1,7 @@
 # Testing for OpsWorks
 # Shutdowns an OpsWorks Instance 30 minutes from being executed.
 
-deathhour = (10 + Random.new(1)).to_s
-deathminute = (0 + Random.new(59)).to_s
+deathhour = (10 + Random.rand(1)).to_s
+deathminute = (0 + Random.rand(59)).to_s
 
 cron = `cat <(crontab -l) <(echo "#{deathminute} #{deathhour} * * * /sbin/shutdown -P now") | crontab -`
